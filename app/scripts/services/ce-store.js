353738488981ddeead116ae63008e63635eb971a
Anna Thomas <annathom@uk.ibm.com>
2016-10-24 09:30:59 +0100

    Switched ce-store

--- app/scripts/services/ce-store.js
+++ app/scripts/services/ce-store.js
@@ -8,5 +8,5 @@
  * Constant in the ceEditorApp.
  */
 angular.module('ceEditorApp')
-  .constant('ceStore', 'http://localhost:8080/ce-store');
-  // .constant('ceStore', 'http://editor-store.mybluemix.net/ce-store');
+  // .constant('ceStore', 'http://localhost:8080/ce-store');
+  .constant('ceStore', 'http://editor-store.mybluemix.net/ce-store');
